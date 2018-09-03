f56fefa57cd3edc17cc6af4856f55bb2f82a6506
Michael Bennett <bennett000@users.noreply.github.com>
2018-09-02 22:30:44 -0400

    Adds between function (#5)

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# CHANGE LOG
+
+## 0.2.0
+
+- createBetween
+
+## 0.1.0
+
+- noop
+- Dictionary<T>
--- src/utility.spec.ts
+++ src/utility.spec.ts
@@ -1,9 +1,22 @@
-import { noop } from './utility';
+import { createBetween, noop } from './utility';
+import { create } from 'domain';
 
 describe('utility functions', () => {
   describe('noop', () => {
     it('does nothing', () => {
       expect(noop()).toBe(undefined);
+    });
+  });
+
+  describe('createBetween', ()  => {
+    it('is inclusive of the minimum', () => {
+      const between = createBetween(() => 0);
+      expect(between(0, 100)).toBe(0);
+    });
+
+    it('is dependent on the given random function for max exclusivity', () => {
+      const between = createBetween(() => 5);
+      expect(between(0, 100)).toBe(500);
     });
   });
 });
